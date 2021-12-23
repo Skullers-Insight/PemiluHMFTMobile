198898e1a40224272e153a4daaf3d9b654f73ffa
Muhammad Sulthan Mazaya <73002733+MSMazaya@users.noreply.github.com>
2021-12-23 16:22:14 +0700

    add animation controller

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -11,6 +11,11 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
+      onModelReady: (model) {
+        Future.delayed(const Duration(milliseconds: 500), () {
+          model.revealHero();
+        });
+      },
       builder: (context, model, child) => Scaffold(
         backgroundColor: AppColors.bg,
         appBar: AppBar(
@@ -19,22 +24,73 @@ class HomeView extends StatelessWidget {
         ),
         drawer: Drawer(),
         body: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              SvgPicture.asset(
-                'lib/assets/images/hero.svg',
-                semanticsLabel: 'Pemilu HMFT',
-              ),
-              AppText(
-                text: "Welcome Skullers",
-                color: AppColors.fontPrimary,
-              ),
-            ],
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Hero(model: model),
+                AppText(
+                  text: model.opacity.toString(),
+                  color: AppColors.fontPrimary,
+                  size: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  width: 20,
+                  height: 3,
+                  color: Colors.amber,
+                ),
+              ],
+            ),
           ),
         ),
       ),
       viewModelBuilder: () => HomeViewModel(),
+    );
+  }
+}
+
+class Hero extends StatelessWidget {
+  final HomeViewModel model;
+  const Hero({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AnimatedOpacity(
+          opacity: model.opacity,
+          duration: const Duration(seconds: 1),
+          child: SvgPicture.asset(
+            'lib/assets/images/hero.svg',
+            semanticsLabel: 'Pemilu HMFT',
+          ),
+        ),
+        AnimatedPositioned(
+          left: model.squareAxis.x,
+          top: model.squareAxis.y,
+          duration: const Duration(seconds: 1),
+          child: Container(
+            width: 100.0,
+            height: 80.0,
+            decoration: BoxDecoration(color: Colors.red),
+            child: Text('hello'),
+          ),
+        ),
+        Positioned(
+          right: 30.0,
+          top: 30.0,
+          child: Container(
+            width: 100.0,
+            height: 80.0,
+            decoration: BoxDecoration(color: Colors.red),
+            child: Text('hello'),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+class Axis {
+  Axis({required this.x, required this.y});
+  double x;
+  double y;
+}
+
 class HomeViewModel extends BaseViewModel {
   String _title = 'Home View';
   String get title => _title;
+  double _opacity = 0;
+  double get opacity => _opacity;
+  Axis _squareAxis = Axis(x: -100, y: 20);
+  Axis get squareAxis => _squareAxis;
+  Axis _circleAxis = Axis(x: -20, y: -20);
+  Axis get circleAxis => _circleAxis;
+  Axis _triangleAxis = Axis(x: -20, y: -20);
+  Axis get triangleAxis => _triangleAxis;
+  void revealHero() {
+    _opacity = 1;
+    _squareAxis = Axis(x: 10, y: 20);
+    _circleAxis = Axis(x: 10, y: 20);
+    _triangleAxis = Axis(x: 10, y: 20);
+    notifyListeners();
+  }
 }
